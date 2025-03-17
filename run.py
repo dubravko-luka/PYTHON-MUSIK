3178f58436420d9887b152f5b358223880518f79
phuongnm <phuongnm@ominext.com>
2025-03-17 13:33:45 +0700

    Update

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from app import create_app
 from app.helpers.helpers import get_env
+import eventlet
 
 app, socketio = create_app()
 
 if __name__ == '__main__':
     if get_env("PYTHON_ENV") == "production":
-        socketio.run(app, host="0.0.0.0", port=get_env("PORT"))
+        eventlet.monkey_patch()
+        socketio.run(app, host="0.0.0.0", port=get_env("PORT"), server='eventlet')
     else:
         socketio.run(app, host="0.0.0.0", port=get_env("PORT"), debug=True)
